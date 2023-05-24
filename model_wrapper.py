3bdd09a5671316e833194aea228715549525d3a7
olivia_zhou <otzhou@bu.edu>
2023-05-23 21:04:17 -0400

    small fixes

--- model_wrapper.py
+++ model_wrapper.py
@@ -11,11 +11,12 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import roc_auc_score, accuracy_score
+from sklearn.metrics import roc_auc_score, accuracy_score, log_loss
 import matplotlib.pyplot as plt
 from dataloader import data
 
 import os, json
+import numpy as np
 
 class wrapper(object):
 
@@ -32,6 +33,8 @@ class wrapper(object):
         self.epochs = hyper_params['max_epochs']
         self.loss_type = hyper_params['loss_type']
         self.batch_size = hyper_params['batch_size']
+        self.cat_idxs = hyper_params['cat_idxs']
+        self.cat_dims = hyper_params['cat_dims']
         self.lr = hyper_params['lr']
 
         # --------------------------------------------------------------------------------------------------------------
@@ -55,8 +58,8 @@ class wrapper(object):
         # --------------------------------------------------------------------------------------------------------------
         #set up model
         self.model = TabNetMultiTaskClassifier(n_steps=1,
-                                        cat_idxs=cat_idxs,
-                                        cat_dims=cat_dims,
+                                        cat_idxs=self.cat_idxs,
+                                        cat_dims=self.cat_dims,
                                         cat_emb_dim=1,
                                         optimizer_fn=torch.optim.Adam,
                                         optimizer_params=dict(lr=self.lr),
@@ -71,16 +74,16 @@ class wrapper(object):
     # train model
     def train(self):
         max_epochs = self.epochs
-        self.model.fit(X_train=X_train, y_train=y_train,
-                    X_valid=X_valid, y_valid=y_valid,
-                    max_epochs=max_epochs ,
-                    patience=50, # please be patient ^^
+        self.model.fit(X_train=x_train, y_train=y_train,
+                    eval_set = [(x_valid, y_valid)],
+                    patience = 50, #T_T a long time
+                    max_epochs=max_epochs,
                     batch_size=1024,
                     virtual_batch_size=128,
                     num_workers=1,
-                    drop_last=False,
+                    drop_last=True
         )
-        # plot losses (drop first epochs to have a nice plot)
+        # plot losses - drop first epoch
         plt.plot(self.model.history['train']['loss'][5:])
         plt.plot(self.model.history['valid']['loss'][5:])
         plt.plot([x for x in self.model.history['train']['lr']][5:])
@@ -88,7 +91,7 @@ class wrapper(object):
     # --------------------------------------------------------------------------------------------------------------
     # validate model
     def validation(self):
-        preds_valid = self.model.predict_proba(X_valid) # This is a list of results for each task
+        preds_valid = self.model.predict_proba(x_valid) # This is a list of results for each task
         # We are here getting rid of tasks where only 0 are available in the validation set
         valid_aucs = [roc_auc_score(y_score=task_pred[:,1], y_true=y_valid[:, task_idx])
                     for task_idx, (task_pred, n_pos) in enumerate(zip(preds_valid, y_valid.sum(axis=0))) if n_pos > 0]
@@ -98,13 +101,26 @@ class wrapper(object):
 
         plt.scatter(y_valid.sum(axis=0)[y_valid.sum(axis=0)>0], valid_aucs)
 
-        # Valid score should match mean log loss - They don't match exactly because we removed some tasks
-        print(f"BEST VALID SCORE FOR {dataset_name} : {clf.best_cost}")
-        print(f"VALIDATION MEAN LOGLOSS SCORES FOR {dataset_name} : {np.mean(valid_logloss)}")
-        print(f"VALIDATION MEAN AUC SCORES FOR {dataset_name} : {np.mean(valid_aucs)}")
+        print(f"BEST VALID SCORE FOR {self.experiment_name} : {self.model.best_cost}")
+        print(f"VALIDATION MEAN LOGLOSS SCORES FOR {self.experiment_name} : {np.mean(valid_logloss)}")
+        print(f"VALIDATION MEAN AUC SCORES FOR {self.experiment_name} : {np.mean(valid_aucs)}")
 
     # --------------------------------------------------------------------------------------------------------------
     # model outputs
     def get_predictions(self):
-        preds = self.model.predict_proba(X_test)
+        preds = self.model.predict_proba(x_test)
         return preds
+    
+    # --------------------------------------------------------------------------------------------------------------
+    # save weights
+    def save_model(self):
+        name = "./model_test_{}".format(self.experiment_name)
+        save_path = self.exp_dir + name
+        saved_filepath = self.model.save_model(save_path)
+        print('successfully saved model weights')
+
+    # --------------------------------------------------------------------------------------------------------------
+    # load checkpoint
+    def load_checkpoint(self, filepath):
+        loaded_model = TabNetMultiTaskClassifier()
+        loaded_model.load_model(filepath)
